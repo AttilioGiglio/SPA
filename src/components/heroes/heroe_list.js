28d613718ca9afdc4bbf4ed9ada22d431ab93386
attiliogiglio <attilio.g93@gmail.com>
2020-10-14 18:50:52 -0300

    adding new files with heroes images

--- src/components/heroes/heroe_list.js
+++ src/components/heroes/heroe_list.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { getHeroesByPublisher } from '../../selectors/get_heroes_by_publisher';
+import HeroeCard from './heroe_card';
 
 const HeroeList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher( publisher );
 
     return (
-        <ul>
+        <div className='card-columns'>
             {
-                heroes.map( hero => (
-                    <li key={hero.id}>
-                        { hero.superhero }
-                    </li>
+                heroes.map( heroe => (
+                    <HeroeCard 
+                    key={heroe.id}
+                    { ...heroe }
+                        />
                 ))
             }
-        </ul>
+        </div>
     )
 }
 
